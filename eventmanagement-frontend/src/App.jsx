b4b8e69ae276a4e26bd71c82fabd4f726c057e83
aliffiazmuhammed <alifmgdi@gmail.com>
2025-01-07 00:44:50 +0530

    shadecn initialised

---
+++ eventmanagement-frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Button>click me</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
+
+export default App
